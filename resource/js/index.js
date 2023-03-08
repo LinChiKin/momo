7709f778bb361011b9df6b3df58cde92a70dfd48
lin chi kin <b11002078@g.chu.edu.tw>
2023-03-09 01:53:20 +0800

    momo

---
+++ resource/js/index.js
@@ -0,0 +1,6 @@
+function type_bar_item_show(xid){
+    document.getElementById(xid).style.left="215px";
+}
+function type_bar_item_unshow(xid){
+     document.getElementById(xid).style.left="9999px";
+}
